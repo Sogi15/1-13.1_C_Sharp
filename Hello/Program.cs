0328da87c104dee563900549b74c6b54ef3637a4
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-07 12:05:38 +0100

    Updated

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,6 +10,27 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            // Ez egy egysoros comment
+            /*
+             Ez többszoros
+             */
+            //konstansok
+            const string FEJLESZTO = "Szunyogh Attila";
+            // valtozok
+            string uzenet = "Hello world";
+            int szam = 10; ;
+            double szam2 = 10.0;
+            bool gender = true;
+            char karakter = 's';
+            string szoveg = "Szöveg";
+            // program törzs
+            Console.WriteLine(uzenet);
+            Console.WriteLine(szam);
+            Console.WriteLine(szam2);
+            Console.WriteLine(gender);
+            Console.WriteLine(karakter);
+            Console.WriteLine(szoveg);
+            Console.ReadKey();
         }
     }
 }
