30150d727002f5cb0196caa9278fa1a48b06fb86
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-07 13:36:26 +0100

    New

---
+++ Mtable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+            if (args.Length > 0)
+            {
+                szam = int.Parse(args[0]);
+            }
+            else
+            {
+                Random rnd = new Random();
+                szam = rnd.Next(1, 50);
+            }
+            Console.WriteLine(szam * 2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Mtable2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int param = args.Length;
+            int szam1 = 0;
+            int szam2 = 0;
+
+            Random rdm = new Random();
+            switch (param)
+            {
+                case 0:
+                    szam1 = rdm.Next(1, 100);
+                    szam2 = rdm.Next(1, 100);
+                    break;
+                case 1:
+                    szam1 = int.Parse(args[0]);
+                    szam2 = rdm.Next(1, 100);
+                    break;
+                case 2:
+                    szam1 = int.Parse(args[0]);
+                    szam2 = int.Parse(args[1]);
+                    break;
+            }
+            Console.WriteLine(szam1 * szam2);
+            Console.ReadKey();
+        }
+    }
+}
