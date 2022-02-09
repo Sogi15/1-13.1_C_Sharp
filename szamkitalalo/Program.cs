c72985190170b29ef84acd64aeb5a2f1e4728d0e
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-09 11:40:55 +0100

    y

--- szamkitalalo/Program.cs
+++ szamkitalalo/Program.cs
@@ -9,93 +9,27 @@ namespace szamkitalalo
     class Program
     {
         static void Main(string[] args)
-        {
-            Random r = new Random();
-            int x = 50;
-            int i = 0;
-            int min = 0;
-            int max = 100;
-            int number = r.Next(100);
-        START:
+        {       
+            Console.WriteLine("Válasz Játékmódot");
+            Console.WriteLine("1. PC dönt");
+            Console.WriteLine("2. Player dönt");
+            int rk = Console.ReadKey(true).KeyChar;
+            if (rk == '1')
+            {
+                Console.WriteLine("AI kezdi a játékot");
+            }
+            else if (rk == '2')
+            {
+                Console.WriteLine("Játékos kezd!");
+            }
+            else
+            {
+                Console.WriteLine("Kérlek 1/2 számot add meg!");
+            }
             
-            Console.WriteLine("Válassz játékmódot!");
-            Console.WriteLine("1 - Te gondolsz egy számra");
-            Console.WriteLine("2 - A számítógép gondol egy számra");
-            
-            switch (Console.ReadKey(true).KeyChar)
-            {
-                case '1': goto PLAYER;
-                case '2': goto COMPUTER;
-            }
+                
 
-        PLAYER:
-            Console.WriteLine("Gondolj egy számra! (1 - 100)");
-            Console.ReadLine();
-            while (i < 5)
-            {
-                Console.WriteLine("A számítógép szerint a szám {0}", x);
-                Console.WriteLine("Szerinted? (k/n/e)");
-                switch (Console.ReadKey(true).KeyChar)
-                {
-                    case 'k':
-                        if (i == 3)
-                            x = r.Next(min, x);
-                        else
-                        {
-                            max = x;
-                            x -= (max - min) / 2;
-                        }
-                        break;
-                    case 'n':
-                        if (i == 3)
-                            x = r.Next(x + 1, max);
-                        else
-                        {
-                            min = x;
-                            x += (max - min) / 2;
-                        }
-                        break;
-                    case 'e':
-                        Console.WriteLine("A számítógép nyert!");
-                        goto END;
-                }
-                ++i;
-            }
-            Console.WriteLine("A számítógép nem tudta kitalálni a számot.");
-            goto END;
-
-
-        COMPUTER:
-            while (i < 5)
-            {
-                Console.WriteLine("\nA tipped: ");
-                x = int.Parse(Console.ReadLine());
-                if (x < number)
-                {
-                    Console.WriteLine("A szám ennél nagyobb!");
-                }
-                else if(x > number)
-                {
-                    Console.WriteLine("A szám ennél kisebb!");
-                }
-                else
-                { Console.WriteLine("Nyertél!");
-                    goto END;
-                }
-                ++i;
-            }
-            Console.WriteLine("\nVesztettél, a szám {0} volt.", number);
-        goto END;
-
-        END:
-            Console.WriteLine("\nAkarsz még játszani? i/n");
-
-            switch (Console.ReadKey(true).KeyChar)
-            {
-                case 'i': goto START;
-                case 'n': break;
-                default: goto END;
-            }
+            Console.ReadKey();
         }
     }
 }
