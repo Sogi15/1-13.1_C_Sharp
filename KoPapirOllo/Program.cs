0a548fa5620d82c6e4ec9624b3946ae2c9974a4f
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-09 09:05:29 +0100

    KPO done

--- KoPapirOllo/Program.cs
+++ KoPapirOllo/Program.cs
@@ -17,56 +17,61 @@ namespace KoPapirOllo
 
             int compScore = 0;
             int playerScore = 0;
-            bool l = true;      
-            Console.WriteLine("Mit választasz? (k/p/o)");
-            switch (Console.ReadKey(true).KeyChar)
-            {
-                case 'k':
-                    playerChoice = "kő";
-                    break;
-                case 'p':
-                    playerChoice = "papír";
-                    break;
-                case 'o':
-                    playerChoice = "olló";
-                    break; }
-            switch (r.Next(0, 3))
-            {
-                case 0:
-                    compChoice = "kő";
-                    break;
-                case 1:
-                    compChoice = "papír";
-                    break;
-                case 2:
-                    compChoice = "olló";
-                    break;
-            }
-            if (
-                (playerChoice == "kő" && compChoice == "papír")
-                ||
-                (playerChoice == "papír" && compChoice == "olló")
-                ||
-                (playerChoice == "olló" && compChoice == "kő")
-                )
-            {
-                Console.WriteLine("Veszítettél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", ++compScore, playerScore);
-            }
-            else if (playerChoice == compChoice)
-            {
-                Console.WriteLine("Döntetlen! Az állás:\nSzámítógép: {0}\nJátékos:{1}", compScore, playerScore);
-            }
-            else
-            {
-                Console.WriteLine("Nyertél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", compScore, ++playerScore);
-            }
+            bool l = true;
             do
             {
+                Console.WriteLine("Mit választasz? (k/p/o)");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'k':
+                        playerChoice = "kő";
+                        break;
+                    case 'p':
+                        playerChoice = "papír";
+                        break;
+                    case 'o':
+                        playerChoice = "olló";
+                        break; 
+                }
+                
+                switch (r.Next(0, 3))
+                {
+                    case 0:
+                        compChoice = "kő";
+                        break;
+                    case 1:
+                        compChoice = "papír";
+                        break;
+                    case 2:
+                        compChoice = "olló";
+                        break;
+                }
+                if (
+                    (playerChoice == "kő" && compChoice == "papír")
+                    ||
+                    (playerChoice == "papír" && compChoice == "olló")
+                    ||
+                    (playerChoice == "olló" && compChoice == "kő")
+                    )
+                {
+                    Console.WriteLine("Veszítettél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", ++compScore, playerScore);
+                }
+                else if (playerChoice == compChoice)
+                {
+                    Console.WriteLine("Döntetlen! Az állás:\nSzámítógép: {0}\nJátékos:{1}", compScore, playerScore);
+                }
+                else
+                {
+                    Console.WriteLine("Nyertél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", compScore, ++playerScore);
+                }
                 Console.WriteLine("Akarsz még játszani? i/n");
 
                 if (Console.ReadKey(true).KeyChar == 'n')
-                    l = false;
+                    {
+                        l = false;
+                    }
             } while (l);
+           
         }
     }
 }
