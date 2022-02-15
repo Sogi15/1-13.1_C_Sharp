206df0a9930e87d4705840b061ad8d7b6f7918be
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-15 14:28:22 +0100

    new

---
+++ teszt_2022_02_15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt_2022_02_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 10;
+            long nagyszam = 1500;
+        }
+    }
+}
