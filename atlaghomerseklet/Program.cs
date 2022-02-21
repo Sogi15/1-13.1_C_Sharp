dbda8ed95af1e5d29939aa2049502b0ba7dd5fc7
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-21 14:15:48 +0100

    new

---
+++ atlaghomerseklet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlaghomerseklet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // változók deklalárása
+            int max = -11,
+                min = 31,
+                atlaghomerseklet = 0,
+                napi = 0,
+                atlagmin = 31,
+                atlagmax = -11,
+                haviatlag = 0,
+                minuszho = 0,
+                minusznap = 0,
+                napok = 0,
+                honapok = 0;
+            
+            int[,] tomb = new int[30,12];
+            int[] atlagok = new int[12];
+        }
+    }
+}
