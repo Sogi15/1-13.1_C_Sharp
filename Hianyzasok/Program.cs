5e41b55d14e1060c590dc59c1ebc681bedac7f33
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-22 11:40:55 +0100

    hianyzas

--- Hianyzasok/Program.cs
+++ Hianyzasok/Program.cs
@@ -10,15 +10,48 @@ namespace Hianyzasok
     {
         static void Main(string[] args)
         {
+            string figy = "Nulla megrovás", //figyelmeztetes dek
+                   jegy = "", //jegy dek
+                    szd = "Nem megadott"; //születési dátum
             int ig = 0; //igazolatlan órák szána
-            DateTime szd; //születési dátum
-            string[] jegyek = new string[] {"Elégtelen (1)", "Elégséges (2)","Közepes (3)","Jó (4)","Jegyes (5)"};
-            ig = int.Parse(Console.ReadLine());
-            if ()
+            string[] jegyek = new string[] {"Rossz (2)", "Változó (3)", "Jó (4)", "Példás (5)"}; //ertekelés
+            string[] figyel = new string[] {"Figyelmeztetés","Osztályfőnöki intő","Igazgatói megrovás","Felfüggesztés"}; //intők
+            Console.WriteLine("kérlek add meg hány nap igazolatlannal rendelkezel!");
+            ig = int.Parse(Console.ReadLine()); //igazolatlan bekérés
+            if (ig >= 50)
             {
-
+                figy = figyel[3];
+            }
+            else if (ig == 30)
+            {
+                figy = figyel[2];
+            }
+            else if (ig == 20)
+            {
+                figy = figyel[1];
+            }
+            else if (ig >= 11)
+            {
+                jegy = jegyek[0];
+            }
+            else if (ig >= 10)
+            {
+                jegy = jegyek[1];
+                figy = figyel[0];
+                Console.WriteLine("Kérlek add meg a születési dátumodat!");
+                szd = Console.ReadLine();
+            }
+            else if (ig == 1 || ig == 2)
+            {
+                jegy = jegyek[2];
+            }
+            else if (ig == 0)
+            {
+                jegy = jegyek[3];
             }
 
+            Console.WriteLine("Igazolatlan hiányzások száma: {0}\nSzületési dátum: {1}\nMagatartási érdemjegyed: {2}\n{3}t kaptál!",ig,szd,jegy,figy);
+            Console.ReadKey();
         }
     }
 }
