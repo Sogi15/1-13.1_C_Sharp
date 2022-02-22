87d5e2ec038fd1e49dabf6d31453cfa8989fc1de
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-22 09:59:36 +0100

    hianyzas

---
+++ Hianyzasok/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hianyzasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ig = 0; //igazolatlan órák szána
+            DateTime szd; //születési dátum
+            string[] jegyek = new string[] {"Elégtelen (1)", "Elégséges (2)","Közepes (3)","Jó (4)","Jegyes (5)"};
+            ig = int.Parse(Console.ReadLine());
+            if ()
+            {
+
+            }
+
+        }
+    }
+}
