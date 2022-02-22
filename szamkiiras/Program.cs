b0db3f7a8dd8caf24a1bba6fe600800fdc9e0096
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-22 15:25:09 +0100

    ds

---
+++ szamkiiras/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamkiiras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+            Console.WriteLine("Kérlek írj be egy számot!");
+            szam = int.Parse(Console.ReadLine());
+            switch (szam)
+            {
+                case 1:
+                    Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
