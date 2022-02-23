a5c9ff67a7246120955f757204456d5930858454
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-23 08:55:53 +0100

    Feladat1 done

---
+++ c_sharp_dolgozat1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_dolgozat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1 = 0,
+                szam2 = 0;
+            Console.WriteLine("Adj meg egy számot!");
+            szam1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második számot!");
+            szam2 = int.Parse(Console.ReadLine());
+            if (szam1 > 0 && szam2 > 0)
+            {
+                Console.WriteLine("A két szám közül egyik sem negatív");
+            }
+            else if (szam1 > 0 && szam2 < 0)
+            {
+                Console.WriteLine("A két szám közül a második negatív");
+            }
+            else if (szam1 < 0 && szam2 > 0)
+            {
+                Console.WriteLine("A két szám közül az első negatív");
+            }
+            else if (szam1 < 0 && szam2 < 0)
+            {
+                Console.WriteLine("Mind a két szám negatív");
+            }
+            Console.ReadKey();
+        }
+    }
+}
