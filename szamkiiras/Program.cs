87f3ec68fb152bb287d852b650900a5ac8dd7e0e
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-23 11:40:06 +0100

    m

--- szamkiiras/Program.cs
+++ szamkiiras/Program.cs
@@ -10,15 +10,36 @@ namespace szamkiiras
     {
         static void Main(string[] args)
         {
-            int szam = 0;
-            string[] ertek1 = new string[] {"egy","kettő","három","négy","öt","hat","hét","nyolc","kilenc","tiz"};
-            string[] ertek2 = new string[] { "tizen", "huszon","harminc","negyven","ötven","hatvan"};
-
-              
+            string szam = "",
+                    szam1 = "";
+            string[] ertek1 = new string[] { "nulla", "egy", "kettő", "három", "négy", "öt", "hat", "hét", "nyolc", "kilenc", "tiz" };
+            string[] ertek2 = new string[] { "tizen", "huszon", "harminc", "negyven", "ötven", "hatvan", "hetven", "nyolcvan", "kilencven" };
+            string[] ertek3 = new string[] { "száz", "kétszáz", "ezer", "millió" };
             Console.WriteLine("Kérlek írj be egy számot!");
-            szam = int.Parse(Console.ReadLine());
-            Console.WriteLine("asd");
+            szam = Console.ReadLine();
+
+            for (int i = 0; i < szam.Length; i++)
+            {
+                if (szam.Length == 1)
+                {
+                    switch (szam)
+                    {
+                        case "0": szam1 = ertek1[0]; break;
+                        case "1": szam1 = ertek1[1]; break;
+                        case "2": szam1 = ertek1[2]; break;
+                        case "3": szam1 = ertek1[3]; break;
+                        case "4": szam1 = ertek1[4]; break;
+                        case "5": szam1 = ertek1[5]; break;
+                        case "6": szam1 = ertek1[6]; break;
+                        case "7": szam1 = ertek1[7]; break;
+                        case "8": szam1 = ertek1[8]; break;
+                        case "9": szam1 = ertek1[9]; break;
+                    }
+                }
+            }
+            Console.WriteLine("{0}", szam1);
             Console.ReadKey();
         }
     }
+
 }
