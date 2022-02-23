4345b290f51ec3aecc4067247aa822cb64a11ee6
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-02-23 10:06:14 +0100

    doga4 done

---
+++ c_sharp_dolgozat4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_dolgozat4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0,
+                nagyobb = 0;
+            do
+            {
+                Console.WriteLine("Írj be egy számot!");
+                szam = int.Parse(Console.ReadLine());
+                if (szam > nagyobb)
+                {
+                    nagyobb = szam;
+                }
+            } while (szam != 0);
+            Console.WriteLine("A legnagyobb szám: {0}",nagyobb);
+            Console.ReadKey();
+        }
+    }
+}
