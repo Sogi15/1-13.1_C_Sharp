36e5d1ce19c7e3c9e28781fead393cc71426bfe2
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-01 11:35:39 +0100

    obj

--- objektumorientaltprogramozas/Program.cs
+++ objektumorientaltprogramozas/Program.cs
@@ -21,6 +21,14 @@ namespace objektumorientaltprogramozas
             this.szin = szin;
         }
 
+        public Allat()
+        {
+            this.nev = "anonim";
+            this.fajta = "ismeretlen";
+            this.szin = "fekete";
+            this.kor = 0;
+        }
+
         public string getnev()
         {
             return this.nev;
@@ -33,6 +41,40 @@ namespace objektumorientaltprogramozas
         {
             return this.szin;
         }
+        public int getkor()
+        {
+            return this.kor;
+        }
+
+        public void setKor(int p)
+        {
+            if (p > 0 && p < 50)
+            {
+                this.kor = p;
+            }
+
+        }
+        public void setNev(string n)
+        {
+            if (n != "")
+            {
+                this.nev = n;
+            }
+        }
+        public void setFajta(string f)
+        {
+            if (f != "")
+            {
+                this.fajta = f;
+            }
+        }
+                public void setSzin(string s)
+        {
+            if (s != "")
+            {
+                this.szin = s;
+            }
+        }
     }
     class Program
     {
@@ -40,10 +82,18 @@ namespace objektumorientaltprogramozas
         {
             // példányosítunk objektumokat
             Allat allat1 = new Allat("Bodri","Puli","Fekete");
-            Console.WriteLine("Az állat neve: {0}", allat1.getnev(),allat1.getfajta());
+            allat1.setKor(2);
+            Console.WriteLine("Az állat neve: {0}\nAz állat fajtája: {1}\nAz állat színe: {2}\nAz állat kora: {3} éves\n", allat1.getnev(),allat1.getfajta(),allat1.getszin(),allat1.getkor());
             Allat allat2 = new Allat("Bolhás", "Komondor", "Fehér");
-            Console.WriteLine("Az állat neve: {0}", allat2.getnev());
-            Console.ReadKey();
+            allat2.setKor(10);
+            Console.WriteLine("Az állat neve: {0}\nAz állat fajtája: {1}\nAz állat színe: {2}\nAz állat kora: {3} éves\n", allat2.getnev(),allat2.getfajta(),allat2.getszin(),allat2.getkor());
+            Allat allat3 = new Allat();
+            allat3.setKor(23);
+            allat3.setNev("CsicskaLángos");
+            allat3.setFajta("Kuvasz");
+            allat3.setSzin("Barna");
+            Console.WriteLine("Az állat neve: {0}\nAz állat fajtája: {1}\nAz állat színe: {2}\nAz állat kora: {3} éves\n", allat3.getnev(),allat3.getfajta(),allat3.getszin(),allat3.getkor());
+          Console.ReadKey();
         }
     }
 }
