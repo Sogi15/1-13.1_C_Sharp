c8a4597e6ea2fd41ec088667426f0c51a1a29cab
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-22 10:00:25 +0100

    kesz

---
+++ homersekletelemzes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homersekletelemzes
+{
+    class Homerseklet {
+        private int[] ho;
+        private string evszak;
+        private string honap;
+        private string[] evszakok = new string[] { "Tél","Tavasz","Nyár","Ősz"};
+        private string[] honapok = new string[] { "Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December" };
+        private int[] hnapok = new int[] {28,30,31};
+        private Random rdm = new Random();
+
+        public void bekeres() {string be }
+        public void datum()
+        {
+            if ()
+
+            //switch (Console.ReadLine())
+            //{
+            //    case "januar": evszak = evszakok[0];honap = honapok[0];
+            //        switch () { }
+            //        break;
+            //}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
