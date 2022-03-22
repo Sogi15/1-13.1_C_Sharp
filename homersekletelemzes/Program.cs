d85bd6303f862a21fc7fc0420b23f13d0aa99bb1
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-22 12:30:10 +0100

    updt

--- homersekletelemzes/Program.cs
+++ homersekletelemzes/Program.cs
@@ -7,18 +7,26 @@ using System.Threading.Tasks;
 namespace homersekletelemzes
 {
     class Homerseklet {
+        private string datum;
         private int[] ho;
         private string evszak;
         private string honap;
-        private string[] evszakok = new string[] { "Tél","Tavasz","Nyár","Ősz"};
+        private string[] evszakok = new string[] { "Tél", "Tavasz", "Nyár", "Ősz" };
         private string[] honapok = new string[] { "Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December" };
-        private int[] hnapok = new int[] {28,30,31};
+        private int[] hnapok = new int[] { 28, 30, 31 };
         private Random rdm = new Random();
-
-        public void bekeres() {string be }
-        public void datum()
+        public Homerseklet(string datum) { this.datum = datum; }
+        public Homerseklet() {}
+        public void bekeres() {
+            Console.WriteLine("Kérlek válassz egy évszakot!!\n1 = Tél\n 2 = Tavasz\n 3 = Nyár\n 4 = Ősz\n");
+            char be = Console.ReadKey().KeyChar;
+            if (be == '1') { Console.WriteLine("Tál"); }
+            else if (be == '2') { Console.WriteLine("Tál"); }
+            else if (be == '3') { Console.WriteLine("Tál"); }
+            else if (be == '4') { Console.WriteLine("Tál"); }
+        }
+        public void date()
         {
-            if ()
 
             //switch (Console.ReadLine())
             //{
@@ -32,6 +40,9 @@ namespace homersekletelemzes
     {
         static void Main(string[] args)
         {
+            Homerseklet megoldas = new Homerseklet();
+            megoldas.bekeres();
+            Console.ReadKey();
 
         }
     }
