fc3c4ea1230e471a26ca4836c01b0ec270cfae9f
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-28 10:50:13 +0200

    wtf

---
+++ 20220328/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220328
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do {
+                Console.WriteLine("Írj be egy számot!");
+                num = int.Parse(Console.ReadLine());
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Karaktert írtál be szám helyett!");
+                }
+            } while (num);
+            Console.ReadKey();
+        }
+    }
+}
