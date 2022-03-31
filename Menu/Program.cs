b91ec0d3b0a51706c6a9cb405d695cd91c5db005
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-31 14:40:32 +0200

    done

--- Menu/Program.cs
+++ Menu/Program.cs
@@ -15,8 +15,28 @@ namespace Menu
         public Menusor() { }
         public void menuk()
         {
-            Console.WriteLine("1| {0}\n2| {1}\n3| {2}\n4| {3}\n5| Kilépés",m1,m2,m3,m4);
-            int szam = Console.ReadKey().KeyChar;
+                Console.WriteLine("1| {0}\n2| {1}\n3| {2}\n4| {3}\n5| Kilépés", m1, m2, m3, m4);
+                int szam = int.Parse(Console.ReadLine());
+                if (szam == 1)
+                {
+                    Console.WriteLine("Válaszott menü:\n{0}", m1);
+                }
+                else if (szam == 2)
+                {
+                    Console.WriteLine("Válaszott menü:\n{0}", m2);
+                }
+                else if (szam == 3)
+                {
+                    Console.WriteLine("Válaszott menü:\n{0}", m3);
+                }
+                else if (szam == 4)
+                {
+                    Console.WriteLine("Válaszott menü:\n{0}", m4);
+                }
+                else if (szam == 5)
+                {
+                    Console.WriteLine("Kilépéshez nyomj entert!");
+                }
         }
     }
     class Program
