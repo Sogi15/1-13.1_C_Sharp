dd9560b11fc4c99554b08e938fd312e39bee5f13
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-03-31 15:40:32 +0200

    done

---
+++ Kettes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 5. Projekt név: Kettes
+// Készítsünk programot, mely tetszőleges tízes számrendszerbeli egész számot átvált kettes számrendszerbe.
+namespace Kettes
+{
+    class Kszam
+    {
+        public Kszam() { }
+        public void kksz()
+        {
+            string eredmeny;
+            Console.WriteLine("Írj be egy számot 10-es számrendszerben!");
+            int bekeres = int.Parse(Console.ReadLine());
+            eredmeny = Convert.ToString(bekeres, 2);
+            Console.WriteLine("Beírt szám kettes számrendszerben:\n{0}", eredmeny);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kszam megold = new Kszam();
+            megold.kksz();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Szamrendszer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamrendszer
+{
+    class Szamrendszer
+    {
+        public string bekeres;
+        public Szamrendszer() { }
+        public void Kettes()
+        {
+            Console.WriteLine("Írj be egy számot!");
+            bekeres =Console.ReadLine();
+            Console.WriteLine("Beírt szám kettes számrendszerben:\n{0}", Convert.ToInt32(bekeres, 2));
+        }
+        public void Nyolcas()
+        {
+            Console.WriteLine("Írj be egy számot!");
+            bekeres = Console.ReadLine();
+            Console.WriteLine("Beírt szám nyolcas számrendszerben:\n{0}", Convert.ToInt32(bekeres, 8));
+        }
+        public void Tizenhatos()
+        {
+            Console.WriteLine("Írj be egy számot!");
+            bekeres = Console.ReadLine();
+            Console.WriteLine("Beírt szám tizenhatos számrendszerben:\n{0}", Convert.ToInt32(bekeres, 16));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valasz;
+            Szamrendszer megold = new Szamrendszer();
+            Console.WriteLine("Válasz hanyas számrendszerbe szeretnél átváltani 10-es számrándszerből! (2/8/16)");
+            valasz = int.Parse(Console.ReadLine());
+            if (valasz == 2)
+            {
+                megold.Kettes();
+            }
+            else if (valasz == 8)
+            {
+                megold.Nyolcas();
+            }
+            else if (valasz == 16)
+            {
+                megold.Tizenhatos();
+            }
+            Console.ReadKey();
+        }
+    }
+}
