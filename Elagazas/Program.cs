fcbc4bb932d2d60011203fa38c400e3365101d85
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-04-04 13:08:58 +0200

    Elagazas done

---
+++ Elagazas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagazas
+{
+    class Elagaz { 
+    public Elagaz() { }
+        private List<int> lista,paros;
+        private Random rdm = new Random();
+        private int[] tomb = new int[] { };
+        public void megold()
+        {
+            lista = new List<int>();
+            paros = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(rdm.Next(0, 100));
+            }
+            Console.WriteLine("Lista elemei:");
+            foreach (var szam in lista)
+            {
+                Console.WriteLine("Elem: {0}", szam);
+                if (szam % 2 == 0)
+                {
+                    paros.Add(szam);
+                }
+            }
+            Console.WriteLine("Páros számok a listában:");
+            foreach (var kiir in paros)
+            {
+                Console.WriteLine("Elem: {0}", kiir);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elagaz megoldas = new Elagaz();
+            megoldas.megold();
+            Console.ReadKey();
+        }
+    }
+}
