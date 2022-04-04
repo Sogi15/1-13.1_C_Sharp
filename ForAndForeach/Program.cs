807888962f2f003e16ea62447dc35d8c52fafc6c
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-04-04 13:24:12 +0200

    Done

---
+++ ForAndForeach/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAndForeach
+{
+    class ForAndForeach
+    {
+        public ForAndForeach() { }
+        private int[] tomb = new int[15];
+        private Random rdm = new Random();
+        public void feltolt()
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rdm.Next(1, 99);
+            }
+        }
+        public void ForC()
+        {
+            Console.WriteLine("=========================================\nTömb kiírása FOR ciklussal\n=========================================");
+            for (int k = 0; k < tomb.Length; k++)
+            {
+                Console.WriteLine(tomb[k]);
+            }
+        }
+        public void ForeachC()
+        {
+            Console.WriteLine("=========================================\nTömb kiírása FOREACH ciklussal\n=========================================");
+            foreach (var index in tomb)
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForAndForeach megold = new ForAndForeach();
+            megold.feltolt();
+            megold.ForC();
+            megold.ForeachC();
+            Console.ReadKey();
+        }
+    }
+}
--- Tukor/Program.cs
+++ Tukor/Program.cs
@@ -11,18 +11,14 @@ namespace Tukor
         static void Main(string[] args)
         {
             string beker, szovegki = String.Empty;
-            Console.WriteLine("írj be egy szót aminek a közepénél tükrözni szeretnéd! (szóköz nélkül!)");
+            Console.WriteLine("írj be egy szót aminek a közepénél tükrözni szeretnéd!");
             String[] cd = { " " };
             beker = Console.ReadLine();
             beker = beker.ToLower();
-            beker = String.Concat(beker.Where(c => !Char.IsWhiteSpace(c)));
-            string first = beker.Substring(0, (int)(beker.Length / 2));
-            string last = beker.Substring((int)(beker.Length / 2), (int)(beker.Length / 2));
-            for (int i = first.Length - 1; i > -1; i--)
+            for (int i = beker.Length - 1; i > -1; i--)
             {
-                szovegki += first[i];
+                szovegki += beker[i];
             }
-            szovegki = szovegki + last;
             Console.WriteLine("A szöveg felénél tükrözve: {0}",szovegki);
             Console.ReadKey();
         }
