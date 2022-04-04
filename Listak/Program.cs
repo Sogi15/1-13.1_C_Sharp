3dcfe1ed1c6e8213b239917a1423f45f15160b81
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-04-04 12:23:47 +0200

    done

---
+++ Listak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Lista
+    {
+        private Random rdm = new Random();
+        private int[] szamok = new int[10];
+        public Lista() { }
+        public void megold()
+        {
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rdm.Next(10, 99);
+            }
+            foreach (var szam in szamok)
+            {
+                Console.WriteLine(szam);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista megoldas = new Lista();
+            megoldas.megold();
+            Console.ReadKey();
+        }
+    }
+}
