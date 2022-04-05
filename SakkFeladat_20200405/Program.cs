6617e9d4623c661f347f455e4f210eb02b032a0a
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-04-05 12:16:32 +0200

    sakk feladat KJ

---
+++ SakkFeladat_20200405/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakkFeladat_20200405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tablaX = new string[8] { "1", "2", "3", "4", "5", "6", "7", "8", },
+                tablaY = new string[8] { "a", "b", "c", "d", "e", "f", "g", "h" };
+            bool bastya = true;
+            string position = string.Empty;
+            if (position == tablaX[7]+tablaY[1])
+            {
+                bastya = true;
+            }
+            else { bastya = false; }
+            
+        }
+    }
+}
