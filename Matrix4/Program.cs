718f0e80ce3f8a7af7aa2af9ede6ba5f0bcd6e99
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-04-12 14:52:27 +0200

    matrix4 done

--- Matrix4/Program.cs
+++ Matrix4/Program.cs
@@ -6,33 +6,88 @@ using System.Threading.Tasks;
 
 namespace Matrix4
 {
+    class Matrixfour
+    {
+        private int sor, oszlop, sidx, oidx, szam;
+        private string beolvas = "";
+        private bool igen = true;
+        private int[,] matrix;
+        private bool[,] tf;
+        public void bevizs()
+        {
+        Console.WriteLine("Írd be a sorok számát!");
+            sor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Írd be az oszlopok számát!");
+            oszlop = int.Parse(Console.ReadLine());
+        matrix = new int[sor, oszlop];
+        tf = new bool[sor, oszlop];
+            do
+            {
+                Console.WriteLine("írj be 3db egyjegyű számot vesszővel elválasztva! (sor,oszlop,érték)");
+                beolvas = Console.ReadLine();
+                if (beolvas == "")
+                {
+                    Console.WriteLine("Kérlek írd be a válaszokat! (sor,oszlop,érték)");
+                    beolvas = Console.ReadLine();
+                }
+                else
+                {
+                    beolvas = String.Concat(beolvas.Where(c => !Char.IsWhiteSpace(c)));
+                    sidx = Convert.ToInt32(beolvas[0].ToString()) - 1;
+                    oidx = Convert.ToInt32(beolvas[2].ToString()) - 1;
+                    szam = Convert.ToInt32(beolvas[4].ToString());
+                    if (sidx > sor || oidx > oszlop)
+                    {
+                        Console.WriteLine("Kérlek az indexekre figyelj oda, hiszen a megadott index a határokon kívülre esett!");
+                    }
+                    else
+                    {
+                        if (tf[sidx, oidx] == true)
+                        {
+                            Console.WriteLine("Már beírtál erre az indexre egy karaktert!");
+                        }
+                        else { matrix[sidx, oidx] = szam; tf[sidx, oidx] = true; }
+
+                    }
+
+                }
+                foreach (var tele in tf)
+                {
+                    if (tele != true)
+                    {
+                        igen = true;
+                    }
+                    else { igen = false; }
+                }
+            } while (igen == true);
+        } 
+        public void kiir()
+        {
+            if (igen == false)
+            {
+                Console.WriteLine("Beírt számok a mátrixba:");
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int k = 0; k < matrix.GetLength(1); k++)
+                    {
+                        Console.Write(matrix[i, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        public Matrixfour() { }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int sor, oszlop;
-            string beolvas = string.Empty;
-            Console.WriteLine("Írd be a sorok számát!");
-            sor = int.Parse(Console.ReadLine());
-            Console.WriteLine("Írd be az oszlopok számát!");
-            oszlop = int.Parse(Console.ReadLine());
-            int[,] matrix = new int[sor, oszlop];
-            Console.WriteLine("írj be 3 egyjegyű számot vesszővel elválasztva!");
-            beolvas = Console.ReadLine();
-            beolvas = String.Concat(beolvas.Where(c => !Char.IsWhiteSpace(c)));
-            // beolvas[0,2,4,]
-            if (beolvas[1] == ',' && beolvas[3] == ',' && beolvas[5] == ',')
-            {
-            }
-            //for (int i = 0; i < matrix.GetLength(0); i++)
-            //{
-            //    for (int k = 0; k < matrix.GetLength(1); k++)
-            //    {
-            //        matrix[]
-            //    }
-            //}
-
+            Matrixfour megold = new Matrixfour();
+            megold.bevizs();
+            megold.kiir();
             Console.ReadKey();
         }
     }
 }
+
