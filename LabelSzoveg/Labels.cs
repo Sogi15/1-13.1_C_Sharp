62746db9dadf2021dcfbc0e69d7ffd98a1af309d
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-05-26 10:50:29 +0200

    update

--- LabelSzoveg/Labels.cs
+++ LabelSzoveg/Labels.cs
@@ -19,7 +19,7 @@ namespace LabelSzoveg
 
         private void Labels_Load(object sender, EventArgs e)
         {
-lbl_full.Text = "Kattints a képernyőn valahova";
+            lbl_full.Text = "Kattints a képernyőn valahova";
         }
 
         private void lbl_full_Click(object sender, EventArgs e)
@@ -33,6 +33,19 @@ lbl_full.Text = "Kattints a képernyőn valahova";
             int o = (int)e.X / (lbl_full.Width / 3);
             // Hanyadik sorba kattintottunk
             int s = (int)e.Y / (lbl_full.Height / 3);
+            switch (s * 3 + o)
+            {
+                case 0: lbl_full.TextAlign = ContentAlignment.TopLeft; break;
+                case 1: lbl_full.TextAlign = ContentAlignment.TopCenter; break;
+                case 2: lbl_full.TextAlign = ContentAlignment.TopRight; break;
+                case 3: lbl_full.TextAlign = ContentAlignment.MiddleLeft; break;
+                case 4: lbl_full.TextAlign = ContentAlignment.MiddleCenter; break;
+                case 5: lbl_full.TextAlign = ContentAlignment.MiddleRight; break;
+                case 6: lbl_full.TextAlign = ContentAlignment.BottomLeft; break;
+                case 7: lbl_full.TextAlign = ContentAlignment.BottomCenter; break;
+                case 8: lbl_full.TextAlign = ContentAlignment.BottomRight; break;
+
+            }
         }
     }
 }
