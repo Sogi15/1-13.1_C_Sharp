d3ef0c5ab09e0f80ddd492ddb534d171c3ec979b
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-05-30 12:57:21 +0200

    form

---
+++ FormMozgatás/formmove.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace FormMozgatás
+{
+    partial class formmove
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_kozep = new System.Windows.Forms.Button();
+            this.btn_fel = new System.Windows.Forms.Button();
+            this.btn_teljesfel = new System.Windows.Forms.Button();
+            this.btn_le = new System.Windows.Forms.Button();
+            this.btn_teljesle = new System.Windows.Forms.Button();
+            this.btn_jobb = new System.Windows.Forms.Button();
+            this.btn_teljesbal = new System.Windows.Forms.Button();
+            this.btn_teljesjobb = new System.Windows.Forms.Button();
+            this.btn_bal = new System.Windows.Forms.Button();
+            this.gb_meret = new System.Windows.Forms.GroupBox();
+            this.btn_tno = new System.Windows.Forms.Button();
+            this.btn_tcsokk = new System.Windows.Forms.Button();
+            this.gb_transparent = new System.Windows.Forms.GroupBox();
+            this.btn_bcsokk = new System.Windows.Forms.Button();
+            this.btn_bnov = new System.Windows.Forms.Button();
+            this.gb_meret.SuspendLayout();
+            this.gb_transparent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_kozep
+            // 
+            this.btn_kozep.BackColor = System.Drawing.Color.Lime;
+            this.btn_kozep.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_kozep.Location = new System.Drawing.Point(323, 194);
+            this.btn_kozep.Name = "btn_kozep";
+            this.btn_kozep.Size = new System.Drawing.Size(100, 30);
+            this.btn_kozep.TabIndex = 0;
+            this.btn_kozep.Text = "Közép";
+            this.btn_kozep.UseVisualStyleBackColor = false;
+            // 
+            // btn_fel
+            // 
+            this.btn_fel.BackColor = System.Drawing.Color.Yellow;
+            this.btn_fel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_fel.Location = new System.Drawing.Point(323, 158);
+            this.btn_fel.Name = "btn_fel";
+            this.btn_fel.Size = new System.Drawing.Size(100, 30);
+            this.btn_fel.TabIndex = 1;
+            this.btn_fel.Text = "Fel";
+            this.btn_fel.UseMnemonic = false;
+            this.btn_fel.UseVisualStyleBackColor = false;
+            // 
+            // btn_teljesfel
+            // 
+            this.btn_teljesfel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_teljesfel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_teljesfel.Location = new System.Drawing.Point(323, 122);
+            this.btn_teljesfel.Name = "btn_teljesfel";
+            this.btn_teljesfel.Size = new System.Drawing.Size(100, 30);
+            this.btn_teljesfel.TabIndex = 2;
+            this.btn_teljesfel.Text = "Teljesen";
+            this.btn_teljesfel.UseMnemonic = false;
+            this.btn_teljesfel.UseVisualStyleBackColor = false;
+            // 
+            // btn_le
+            // 
+            this.btn_le.BackColor = System.Drawing.Color.Yellow;
+            this.btn_le.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_le.Location = new System.Drawing.Point(323, 230);
+            this.btn_le.Name = "btn_le";
+            this.btn_le.Size = new System.Drawing.Size(100, 30);
+            this.btn_le.TabIndex = 3;
+            this.btn_le.Text = "Le";
+            this.btn_le.UseVisualStyleBackColor = false;
+            // 
+            // btn_teljesle
+            // 
+            this.btn_teljesle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_teljesle.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_teljesle.Location = new System.Drawing.Point(323, 266);
+            this.btn_teljesle.Name = "btn_teljesle";
+            this.btn_teljesle.Size = new System.Drawing.Size(100, 30);
+            this.btn_teljesle.TabIndex = 4;
+            this.btn_teljesle.Text = "Teljesen";
+            this.btn_teljesle.UseVisualStyleBackColor = false;
+            // 
+            // btn_jobb
+            // 
+            this.btn_jobb.BackColor = System.Drawing.Color.Yellow;
+            this.btn_jobb.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_jobb.Location = new System.Drawing.Point(429, 194);
+            this.btn_jobb.Name = "btn_jobb";
+            this.btn_jobb.Size = new System.Drawing.Size(100, 30);
+            this.btn_jobb.TabIndex = 5;
+            this.btn_jobb.Text = "Jobbra";
+            this.btn_jobb.UseVisualStyleBackColor = false;
+            // 
+            // btn_teljesbal
+            // 
+            this.btn_teljesbal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_teljesbal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_teljesbal.Location = new System.Drawing.Point(111, 194);
+            this.btn_teljesbal.Name = "btn_teljesbal";
+            this.btn_teljesbal.Size = new System.Drawing.Size(100, 30);
+            this.btn_teljesbal.TabIndex = 6;
+            this.btn_teljesbal.Text = "Teljesen";
+            this.btn_teljesbal.UseVisualStyleBackColor = false;
+            // 
+            // btn_teljesjobb
+            // 
+            this.btn_teljesjobb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_teljesjobb.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_teljesjobb.Location = new System.Drawing.Point(535, 194);
+            this.btn_teljesjobb.Name = "btn_teljesjobb";
+            this.btn_teljesjobb.Size = new System.Drawing.Size(100, 30);
+            this.btn_teljesjobb.TabIndex = 6;
+            this.btn_teljesjobb.Text = "Teljesen";
+            this.btn_teljesjobb.UseVisualStyleBackColor = false;
+            // 
+            // btn_bal
+            // 
+            this.btn_bal.BackColor = System.Drawing.Color.Yellow;
+            this.btn_bal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_bal.Location = new System.Drawing.Point(217, 194);
+            this.btn_bal.Name = "btn_bal";
+            this.btn_bal.Size = new System.Drawing.Size(100, 30);
+            this.btn_bal.TabIndex = 7;
+            this.btn_bal.Text = "Balra";
+            this.btn_bal.UseVisualStyleBackColor = false;
+            // 
+            // gb_meret
+            // 
+            this.gb_meret.BackColor = System.Drawing.Color.Maroon;
+            this.gb_meret.Controls.Add(this.btn_tcsokk);
+            this.gb_meret.Controls.Add(this.btn_tno);
+            this.gb_meret.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gb_meret.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.gb_meret.Location = new System.Drawing.Point(217, 36);
+            this.gb_meret.Name = "gb_meret";
+            this.gb_meret.Size = new System.Drawing.Size(312, 80);
+            this.gb_meret.TabIndex = 8;
+            this.gb_meret.TabStop = false;
+            this.gb_meret.Text = "Méret";
+            // 
+            // btn_tno
+            // 
+            this.btn_tno.BackColor = System.Drawing.Color.Red;
+            this.btn_tno.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_tno.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_tno.Location = new System.Drawing.Point(6, 31);
+            this.btn_tno.Name = "btn_tno";
+            this.btn_tno.Size = new System.Drawing.Size(100, 43);
+            this.btn_tno.TabIndex = 9;
+            this.btn_tno.Text = "Növekszik";
+            this.btn_tno.UseVisualStyleBackColor = false;
+            // 
+            // btn_tcsokk
+            // 
+            this.btn_tcsokk.BackColor = System.Drawing.Color.Red;
+            this.btn_tcsokk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_tcsokk.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_tcsokk.Location = new System.Drawing.Point(206, 31);
+            this.btn_tcsokk.Name = "btn_tcsokk";
+            this.btn_tcsokk.Size = new System.Drawing.Size(100, 43);
+            this.btn_tcsokk.TabIndex = 10;
+            this.btn_tcsokk.Text = "Csökken";
+            this.btn_tcsokk.UseVisualStyleBackColor = false;
+            // 
+            // gb_transparent
+            // 
+            this.gb_transparent.BackColor = System.Drawing.Color.Maroon;
+            this.gb_transparent.Controls.Add(this.btn_bcsokk);
+            this.gb_transparent.Controls.Add(this.btn_bnov);
+            this.gb_transparent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gb_transparent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.gb_transparent.Location = new System.Drawing.Point(217, 302);
+            this.gb_transparent.Name = "gb_transparent";
+            this.gb_transparent.Size = new System.Drawing.Size(312, 80);
+            this.gb_transparent.TabIndex = 11;
+            this.gb_transparent.TabStop = false;
+            this.gb_transparent.Text = "Átlátszóság";
+            // 
+            // btn_bcsokk
+            // 
+            this.btn_bcsokk.BackColor = System.Drawing.Color.Red;
+            this.btn_bcsokk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_bcsokk.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_bcsokk.Location = new System.Drawing.Point(206, 31);
+            this.btn_bcsokk.Name = "btn_bcsokk";
+            this.btn_bcsokk.Size = new System.Drawing.Size(100, 43);
+            this.btn_bcsokk.TabIndex = 10;
+            this.btn_bcsokk.Text = "Csökken";
+            this.btn_bcsokk.UseVisualStyleBackColor = false;
+            // 
+            // btn_bnov
+            // 
+            this.btn_bnov.BackColor = System.Drawing.Color.Red;
+            this.btn_bnov.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_bnov.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_bnov.Location = new System.Drawing.Point(6, 31);
+            this.btn_bnov.Name = "btn_bnov";
+            this.btn_bnov.Size = new System.Drawing.Size(100, 43);
+            this.btn_bnov.TabIndex = 9;
+            this.btn_bnov.Text = "Növekszik";
+            this.btn_bnov.UseVisualStyleBackColor = false;
+            // 
+            // formmove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gb_transparent);
+            this.Controls.Add(this.gb_meret);
+            this.Controls.Add(this.btn_bal);
+            this.Controls.Add(this.btn_teljesjobb);
+            this.Controls.Add(this.btn_teljesbal);
+            this.Controls.Add(this.btn_jobb);
+            this.Controls.Add(this.btn_teljesle);
+            this.Controls.Add(this.btn_le);
+            this.Controls.Add(this.btn_teljesfel);
+            this.Controls.Add(this.btn_fel);
+            this.Controls.Add(this.btn_kozep);
+            this.Name = "formmove";
+            this.Text = "FormMove";
+            this.Load += new System.EventHandler(this.formmove_Load);
+            this.gb_meret.ResumeLayout(false);
+            this.gb_transparent.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kozep;
+        private System.Windows.Forms.Button btn_fel;
+        private System.Windows.Forms.Button btn_teljesfel;
+        private System.Windows.Forms.Button btn_le;
+        private System.Windows.Forms.Button btn_teljesle;
+        private System.Windows.Forms.Button btn_jobb;
+        private System.Windows.Forms.Button btn_teljesbal;
+        private System.Windows.Forms.Button btn_teljesjobb;
+        private System.Windows.Forms.Button btn_bal;
+        private System.Windows.Forms.GroupBox gb_meret;
+        private System.Windows.Forms.Button btn_tcsokk;
+        private System.Windows.Forms.Button btn_tno;
+        private System.Windows.Forms.GroupBox gb_transparent;
+        private System.Windows.Forms.Button btn_bcsokk;
+        private System.Windows.Forms.Button btn_bnov;
+    }
+}
+
