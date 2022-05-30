1ec1c7950e4d452eb1fa03c56c59e5246e54b4a9
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-05-30 14:27:30 +0200

    szigetekUt

---
+++ SzigetekUT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzigetekUT
+{
+    public class Island
+    {
+        private string cords;
+        public Island(string cords)
+        {
+            this.cords = cords;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
