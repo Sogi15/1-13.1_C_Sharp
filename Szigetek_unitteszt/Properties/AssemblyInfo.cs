71315a40272ef71648f3ed946c4446d5c2abd0fc
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-05-31 09:28:54 +0200

    Sziget_unitteszt done

---
+++ Szigetek_unitteszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Szigetek_unitteszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Szigetek_unitteszt")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c87dcb89-ae24-4fe9-9342-1a7f3b12323e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Szigetek_unitteszt/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SzigetekUT;
+
+namespace Szigetek_unitteszt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static string Cords = "0011111111111111100110101110110011111111011101110111"; //15-9
+        Island megold = new Island(Cords);
+        [TestMethod]
+        public void TestSzigetCount()
+        {
+            //Arrange
+            var vart = 9;
+            //ACT
+            var kapott = megold.SzN();
+            //Assert
+            Assert.AreEqual(vart, kapott);
+        }
+        [TestMethod]
+        public void TestSzigetLength()
+        {
+            //Arrange
+            var vart = 15;
+            //ACT
+            var kapott = megold.SzH();
+            //Assert
+            Assert.AreEqual(vart, kapott);
+
+        }
+    }
+}
